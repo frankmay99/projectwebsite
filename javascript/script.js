fe139648417bde39e5e85e02b6d88e245ecbc676
frankmay99 <74114409+frankmay99@users.noreply.github.com>
2021-08-11 20:40:14 +0100

    home page and links completed

--- javascript/script.js
+++ javascript/script.js
@@ -10,8 +10,6 @@ function mytest() {
     httpRequest.send();
 }
 
-
-
 function alertContents() {
     if (httpRequest.readyState === XMLHttpRequest.DONE) {
         if (httpRequest.status === 200) {
@@ -25,3 +23,60 @@ function alertContents() {
     }
 }
 
+function switchToWhite(item) {
+    item.style.color = "white";
+}
+
+function switchBack(item) {
+    item.style.color = "black";
+}
+
+var link1 = document.getElementById("about_link");
+var link2 = document.getElementById("seperate_link");
+var link3 = document.getElementById("translate_link");
+var link4 = document.getElementById("covid_link");
+var link5 = document.getElementById("style_link");
+var link6 = document.getElementById("crypto_link");
+var caption1 = document.getElementById("about_caption");
+var caption2 = document.getElementById("seperate_caption");
+var caption3 = document.getElementById("translate_caption");
+var caption4 = document.getElementById("covid_caption");
+var caption5 = document.getElementById("style_caption");
+var caption6 = document.getElementById("crypto_caption");
+
+link1.addEventListener("mouseover", function() {
+    switchToWhite(caption1);
+});
+link2.addEventListener("mouseover", function() {
+    switchToWhite(caption2);
+});
+link3.addEventListener("mouseover", function() {
+    switchToWhite(caption3);
+});
+link4.addEventListener("mouseover", function() {
+    switchToWhite(caption4);
+});
+link5.addEventListener("mouseover", function() {
+    switchToWhite(caption5);
+});
+link6.addEventListener("mouseover", function() {
+    switchToWhite(caption6);
+});
+link1.addEventListener("mouseleave", function() {
+    switchBack(caption1);
+});
+link2.addEventListener("mouseleave", function() {
+    switchBack(caption2);
+});
+link3.addEventListener("mouseleave", function() {
+    switchBack(caption3);
+});
+link4.addEventListener("mouseleave", function() {
+    switchBack(caption4);
+});
+link5.addEventListener("mouseleave", function() {
+    switchBack(caption5);
+});
+link6.addEventListener("mouseleave", function() {
+    switchBack(caption6);
+});
